e0b58bf67c4c5d312744214d67cdc2b5eaa3f60a
David de Hilster <david.dehilster@lexisnexisrisk.com>
2021-02-28 11:24:22 -0500

    UH-ADOBE-006 Updated scripts
    Signed-off-by: David de Hilster <david.dehilster@lexisnexisrisk.com>

--- LeSageTwoBody.jsx
+++ LeSageTwoBody.jsx
@@ -1,12 +1,18 @@
 #include "TPM.jsx";
 
-var number = Number(prompt("Number of G1s","20"));
+var number = Number(prompt("Number of path lines","10"));
 if (number > 0) {
-    var objectColor = prompt("Object Color?","gray");
-    var gColor = prompt("Field Particle Color?","lightblue");
+    var objectColor = prompt("Object Color?","lightblue");
+    var impactColor = prompt("Impact Particle Color?","gray");
+    var passColor = prompt("Passthru Particle Color?","black");
     var width = 30;
     var height = 15;
     var size = 20;
+    var spacing = 20;
+    var reduction = 2;
+    var gridText = true;
+    var canvasOnly = true;
+    var drawLines = false;
     var object = [
             127,128,129,130,               141,142,143,144,
         156,157,158,159,160,161,       170,171,172,173,174,175,
@@ -16,9 +22,7 @@ if (number > 0) {
             277,278,279,280,               291,292,293,294,
     ];
 
-
     var group_field = getDoc().groupItems.add();
 
-    graviticCanvasField(group_field,number,width,height,size,object,objectColor,gColor,false);   
+    graviticCanvasField(group_field,number,width,height,size,spacing,object,reduction,objectColor,impactColor,passColor,gridText,canvasOnly,drawLines);   
 }
-
